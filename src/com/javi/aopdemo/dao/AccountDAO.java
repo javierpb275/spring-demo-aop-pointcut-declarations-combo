79673bb1472b5cb778b2cc1fd0fbb00367272543
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-07-16 18:04:31 +0200

    created variables and getters and setters in AccountDAO

--- src/com/javi/aopdemo/dao/AccountDAO.java
+++ src/com/javi/aopdemo/dao/AccountDAO.java
@@ -7,6 +7,9 @@ import com.javi.aopdemo.Account;
 @Component
 public class AccountDAO {
 	
+	private String name;
+	private String serviceCode;
+	
 	public void addAccount(Account theAccount, boolean vipFlag) {
 		
 		System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
@@ -19,5 +22,29 @@ public class AccountDAO {
 		return false;
 		
 	}
+	
+	//getters and setters
+
+	public String getName() {
+		System.out.println(getClass() + ": in getName()");
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println(getClass() + ": in setName()");
+		this.name = name;
+	}
+
+	public String getServiceCode() {
+		System.out.println(getClass() + ": in getServiceCode");
+		return serviceCode;
+	}
+
+	public void setServiceCode(String serviceCode) {
+		System.out.println(getClass() + ": in setServiceCode");
+		this.serviceCode = serviceCode;
+	}
+	
+	
 
 }
